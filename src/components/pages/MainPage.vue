4a25a30e6fb4437e33f07f2d28f6bde5b18dc562
strukovd <dmitryi97@yandex.ru>
2026-09-03 03:00:24 +0600

    use toolbar from separated components

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -2,23 +2,7 @@
 	<header></header>
 	<main class="main-screen">
 		<RouterView/>
-		<section id="tool-bar">
-			<div>circle</div>
-			<div>line</div>
-			<div>figure</div>
-			<div>path</div>
-			<div title="moveto (move from one point to another point)">M</div>
-			<div title="lineto (create a line)">L</div>
-			<div title="horizontal lineto (create a horizontal line)">H</div>
-			<div title="vertical lineto (create a vertical line)">V</div>
-			<div title="curveto (create a curve)">C</div>
-			<div title="smooth curveto (create a smooth curve)">S</div>
-			<div title="quadratic Bézier curve (create a quadratic Bézier curve)">Q</div>
-			<div title="smooth quadratic Bézier curveto (create a smooth quadratic Bézier curve)">T</div>
-			<div title="elliptical Arc (create a elliptical arc)">A</div>
-			<div title="closepath (close the path)">Z</div>
-			...<br>
-		</section>
+		<ToolBar/>
 		<div id="workspace">
 			<SceneComponent/>
 		</div>
@@ -32,15 +16,13 @@
 <script lang="ts" setup>
 import SceneComponent from '@/components/common/scene/SceneComponent.vue';
 import PropsBar from '../common/scene/PropsBar.vue';
+import ToolBar from '../common/scene/ToolBar.vue';
 </script>
 
 <style lang="scss">
 .main-screen {
 	display: flex;
 
-	#tool-bar {
-		max-width:3em;
-	}
 	#workspace {
 		flex:auto 1 0;
 	}
